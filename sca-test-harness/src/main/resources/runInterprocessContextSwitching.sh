791384f86552b4cb326a5226e47e724fdb639da0
mbo <mbo@octo.com>
2013-06-25 00:15:09 +0200

    Add missing sh

---
+++ sca-test-harness/src/main/resources/runInterprocessContextSwitching.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -L $0 ] ; then
+    DIR=$(dirname $(readlink -f $0)) ;
+else
+    DIR=$(dirname $0) ;
+fi ;
+
+AUTH_HOST=127.0.0.1
+HOD_HOST=127.0.0.1
+TEST_CSV=InterprocessContextSwitchingExercise.csv
+TEST_HARNESS_VERSION=0.1.0.BUILD-SNAPSHOT
+
+java -cp $DIR/../../../target/sca-test-harness-$TEST_HARNESS_VERSION-jar-with-dependencies.jar -DJMX_AUTH_SERVER_HOST=$AUTH_HOST -Djclarity.hod.host=$HOD_HOST  com.jclarity.had_one_dismissal.Main -f $TEST_CSV
