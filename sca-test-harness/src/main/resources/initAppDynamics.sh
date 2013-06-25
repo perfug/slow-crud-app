f9acaea0eea66f0da8355e88880522234f99cd6c
Ubuntu <ubuntu@ip-10-49-93-245.eu-west-1.compute.internal>
2013-06-25 21:47:17 +0000

    Correct script

--- sca-test-harness/src/main/resources/initAppDynamics.sh
+++ sca-test-harness/src/main/resources/initAppDynamics.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-echo Startinct "Slow-crud-app_$HOSTNAME"
+echo Starting "Slow-crud-app_`hostname`"
 service tomcat7-auth stop
 service tomcat7-had_one_dismissal stop
 killall java
@@ -10,19 +8,25 @@ rm -Rf /opt/appdynamics/AppServerAgent1/logs/*
 rm -Rf /opt/appdynamics/AppServerAgent2/logs/*
 rm -Rf /opt/appdynamics/MachineAgent/logs/*
 
-sed -i -e "s/<application-name>.*<\/application-name>/<application-name>slow-crud-app_$HOSTNAME<\/application-name>/" /opt/appdynamics/AppServerAgent1/conf/controller-info.xml
-sed -i -e "s/<node-name>.*<\/node-name>/<node-name>$HOSTNAME<\/node-name>/" /opt/appdynamics/AppServerAgent1/conf/controller-info.xml
+sed -i -e "s/<application-name>.*<\/application-name>/<application-name>slow-crud-app_`hostname`<\/application-name>/" /opt/appdynamics/AppServerAgent1/conf/controller-info.xml
+sed -i -e "s/<node-name>.*<\/node-name>/<node-name>`hostname`<\/node-name>/" /opt/appdynamics/AppServerAgent1/conf/controller-info.xml
 
-sed -i -e "s/<application-name>.*<\/application-name>/<application-name>slow-crud-app_$HOSTNAME<\/application-name>/" /opt/appdynamics/AppServerAgent2/conf/controller-info.xml
-sed -i -e "s/<node-name>.*<\/node-name>/<node-name>$HOSTNAME<\/node-name>/" /opt/appdynamics/AppServerAgent2/conf/controller-info.xml
+sed -i -e "s/<application-name>.*<\/application-name>/<application-name>slow-crud-app_`hostname`<\/application-name>/" /opt/appdynamics/AppServerAgent2/conf/controller-info.xml
+sed -i -e "s/<node-name>.*<\/node-name>/<node-name>`hostname`<\/node-name>/" /opt/appdynamics/AppServerAgent2/conf/controller-info.xml
 
-sed -i -e "s/<application-name>.*<\/application-name>/<application-name>slow-crud-app_$HOSTNAME<\/application-name>/" /opt/appdynamics/MachineAgent/conf/controller-info.xml
-sed -i -e "s/<node-name>.*<\/node-name>/<node-name>$HOSTNAME<\/node-name>/" /opt/appdynamics/MachineAgent/conf/controller-info.xml
+sed -i -e "s/<application-name>.*<\/application-name>/<application-name>slow-crud-app_`hostname`<\/application-name>/" /opt/appdynamics/MachineAgent/conf/controller-info.xml
+sed -i -e "s/<node-name>.*<\/node-name>/<node-name>`hostname`<\/node-name>/" /opt/appdynamics/MachineAgent/conf/controller-info.xml
 
 service tomcat7-auth start
-service tomcat6-had_one_dismissal start
+service tomcat7-had_one_dismissal start
 
-java -jar /opt/appdynamics/MachineAgent/machineagent.jar &
+java -jar /opt/appdynamics/MachineAgent/machineagent.jar >$ /opt/appdynamics/MachinesAgent/logs/out.log &
+
+#Have to init webapp to activate JMW
+echo "Please wait..."
+sleep 60
+wget http://localhost:8080/had_one_dismissal/ > /dev/null
+wget http://localhost:9876/auth/ > /dev/null
 
 echo "Started"
 
